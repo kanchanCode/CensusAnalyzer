9c4b9c7883949ef3f56a2b1d75ffeef7094b0a33
kanchanCode <jkanchan2911@gmail.com>
2021-11-24 17:06:31 +0530

    Class with CSVreader to read data for UC1

---
+++ AnalyzeCensusData.cs
@@ -0,0 +1,35 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace censusAnalyzerTest
+{
+    class AnalyzeCensusData
+    {
+
+        public AnalyzeCensusData()
+        {
+
+        }
+
+        public List<IndianStateCensusDataClass> loadCensusData(string filePath)
+        {
+
+            StreamReader streamReader = File.OpenText(filePath);
+            CsvReader csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+            return csvReader.GetRecords<IndianStateCensusDataClass>().ToList();
+
+        }
+        public List<IndianStateCodeClass> loadCensusStateCode(string filePath)
+        {
+
+            StreamReader streamReader = File.OpenText(filePath);
+            CsvReader csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+            return csvReader.GetRecords<IndianStateCodeClass>().ToList();
+
+        }
+    }
+}
