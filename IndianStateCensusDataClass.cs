69da70b524ce110ecd2334fcb09e825cdbd9154b
kanchanCode <jkanchan2911@gmail.com>
2021-11-24 17:01:02 +0530

    Added Test file to check for records for UC1

---
+++ IndianStateCensusDataClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace censusAnalyzerTest
+{
+    public class IndianStateCensusDataClass
+    {
+        public string State { get; set; }
+        public long Population { get; set; }
+        public long AreaInSqKm { get; set; }
+        public long DensityPerSqKm { get; set; }
+
+    }
+}
